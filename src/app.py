f83d6480ca36f817f9718d919e7688a46f069137
ClaxxArtik <claxxgamer948@gmail.com>
2026-07-11 23:57:25 -0500

    Classes and Functions

--- src/app.py
+++ src/app.py
@@ -2,55 +2,53 @@ import downloader as dw
 import tkinter as tk
 from tkinter import ttk
 
-root = tk.Tk()
-root.geometry("900x600")
-root.resizable(False, False)
-root.title("CryoDownloader")
 
-# Main container frame that will center everything
-main_frame = ttk.Frame(root)
-main_frame.pack(expand=True)
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-# Your widgets inside main_frame
-main_text = ttk.Label(main_frame, text="Youtube URL:")
-main_text.pack(pady=10)
+        # configure the root window
+        self.title("CryoDownloader")
+        self.geometry("900x600")
+        self.resizable(False, False)
 
-url_entry = tk.Entry(main_frame, width=60, justify="center")
-url_entry.pack()
+        # frame
+        self.frame = ttk.Frame(self)
+        self.frame.pack(expand=True)
 
-button_frame = ttk.Frame(main_frame)
-button_frame.pack(pady=20)
+        # label
+        self.label = ttk.Label(self.frame, text="Youtube URL:")
+        self.label.pack(pady=10)
 
-# open audio button
-mp3_dir_button = ttk.Button(
-    button_frame, text="📁", command=lambda: dw.open_audio_dir(), width=3
-)
+        # url entry
+        self.url = ttk.Entry(self.frame, width=60, justify="center")
+        self.url.pack()
 
-mp3_dir_button.pack(ipadx=5, ipady=5, side=tk.LEFT, padx=(10, 5))
+        # button frame
+        self.button_frame = ttk.Frame(self.frame)
+        self.button_frame.pack(pady=20)
 
-# download audio button
-mp3_button = ttk.Button(
-    button_frame,
-    text="Download Audio",
-    command=lambda: dw.download_audio(url_entry.get()) if url_entry.get() else None,
-)
+        # audio directory button
+        self.audio_dir_button = ttk.Button(self.button_frame, text="📁", width=3)
+        self.audio_dir_button["command"] = dw.Open.audio
+        self.audio_dir_button.pack(ipadx=5, ipady=5, side=tk.LEFT, padx=(10, 5))
 
-mp3_button.pack(ipadx=5, ipady=5, side=tk.LEFT, padx=(5, 5))
+        # download audio button
+        self.dw_audio_button = ttk.Button(self.button_frame, text="Download audio")
+        self.dw_audio_button["command"] = lambda: dw.Download.audio(self.url.get())
+        self.dw_audio_button.pack(ipadx=5, ipady=5, side=tk.LEFT, padx=(5, 5))
 
-# download video button
-mp4_button = ttk.Button(
-    button_frame,
-    text="Download Video",
-    command=lambda: dw.download_video(url_entry.get()) if url_entry.get() else None,
-)
+        # download video button
+        self.dw_video_button = ttk.Button(self.button_frame, text="Download video")
+        self.dw_video_button["command"] = lambda: dw.Download.video(self.url.get())
+        self.dw_video_button.pack(ipadx=5, ipady=5, side=tk.LEFT, padx=(5, 5))
 
-mp4_button.pack(ipadx=5, ipady=5, side=tk.LEFT, padx=(5, 5))
+        # video directory button
+        self.video_dir_button = ttk.Button(self.button_frame, text="📁", width=3)
+        self.video_dir_button["command"] = dw.Open.video
+        self.video_dir_button.pack(ipadx=5, ipady=5, side=tk.LEFT, padx=(10, 5))
 
-# open video button
-mp4_dir_button = ttk.Button(
-    button_frame, text="📁", command=lambda: dw.open_video_dir(), width=3
-)
 
-mp4_dir_button.pack(ipadx=5, ipady=5, side=tk.LEFT, padx=(5, 10))
-
-root.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
--- src/downloader.py
+++ src/downloader.py
@@ -1,64 +1,72 @@
 import yt_dlp
 import os
+import threading
+import time
 
 
-def download_audio(url):
+class Download:
 
-    audio_dir = os.path.join(
-        "downloads", "audio"
-    )  # Set the directory for audio downloads
-    os.makedirs(audio_dir, exist_ok=True)  # Create the directory if it doesn't exist
+    def audio(url):
 
-    ydl_audio = {
-        "format": "mp3/bestaudio/best",
-        "outtmpl": os.path.join(
-            audio_dir, "%(title)s.%(ext)s"
-        ),  # Save to downloads/audio folder with title as filename
-        "postprocessors": [
-            {  # Extract audio using ffmpeg
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-            },
-            {"key": "EmbedThumbnail"},
-        ],
-        "writethumbnail": True,
-    }
+        audio_dir = os.path.join(
+            "downloads", "audio"
+        )  # Set the directory for audio downloads
+        os.makedirs(
+            audio_dir, exist_ok=True
+        )  # Create the directory if it doesn't exist
 
-    with yt_dlp.YoutubeDL(ydl_audio) as ydl:
-        error_code = ydl.download(url)
+        ydl_audio = {
+            "format": "mp3/bestaudio/best",
+            "outtmpl": os.path.join(
+                audio_dir, "%(title)s.%(ext)s"
+            ),  # Save to downloads/audio folder with title as filename
+            "postprocessors": [
+                {  # Extract audio using ffmpeg
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                },
+                {"key": "EmbedThumbnail"},
+            ],
+            "writethumbnail": True,
+        }
+
+        with yt_dlp.YoutubeDL(ydl_audio) as ydl:
+            error_code = ydl.download(url)
+
+    def video(url):
+
+        video_dir = os.path.join(
+            "downloads", "video"
+        )  # Set the directory for video downloads
+        os.makedirs(
+            video_dir, exist_ok=True
+        )  # Create the directory if it doesn't exist
+
+        ydl_video = {
+            "format": "bestvideo[height<=1080]+bestaudio/best",
+            "outtmpl": os.path.join(
+                video_dir, "%(title)s.%(ext)s"
+            ),  # Save to downloads/video folder with title as filename
+            "postprocessors": [
+                {  # Extract audio using ffmpeg
+                    "key": "FFmpegVideoConvertor",
+                    "preferedformat": "mp4",
+                }
+            ],
+        }
+
+        with yt_dlp.YoutubeDL(ydl_video) as ydl:
+            error_code = ydl.download(url)
 
 
-def download_video(url):
+class Open:
 
-    video_dir = os.path.join(
-        "downloads", "video"
-    )  # Set the directory for video downloads
-    os.makedirs(video_dir, exist_ok=True)  # Create the directory if it doesn't exist
+    def audio():
+        audio_dir = os.path.join("downloads", "audio")
+        os.makedirs(audio_dir, exist_ok=True)
+        os.startfile(audio_dir)
 
-    ydl_video = {
-        "format": "bestvideo[height<=1080]+bestaudio/best",
-        "outtmpl": os.path.join(
-            video_dir, "%(title)s.%(ext)s"
-        ),  # Save to downloads/video folder with title as filename
-        "postprocessors": [
-            {  # Extract audio using ffmpeg
-                "key": "FFmpegVideoConvertor",
-                "preferedformat": "mp4",
-            }
-        ],
-    }
-
-    with yt_dlp.YoutubeDL(ydl_video) as ydl:
-        error_code = ydl.download(url)
-
-
-def open_audio_dir():
-    audio_dir = os.path.join("downloads", "audio")
-    os.makedirs(audio_dir, exist_ok=True)
-    os.startfile(audio_dir)
-
-
-def open_video_dir():
-    video_dir = os.path.join("downloads", "video")
-    os.makedirs(video_dir, exist_ok=True)
-    os.startfile(video_dir)
+    def video():
+        video_dir = os.path.join("downloads", "video")
+        os.makedirs(video_dir, exist_ok=True)
+        os.startfile(video_dir)
